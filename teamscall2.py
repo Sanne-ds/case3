61d8debda503ae8bca9df458373ab4d97b062d16
Sanne-ds <sanne.groenland@hva.nl>
2025-04-03 18:36:35 +0200

    Update teamscall2.py

--- teamscall2.py
+++ teamscall2.py
@@ -8,33 +8,6 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import plotly.express as px
 import plotly.graph_objects as go
-# JS code to modify te decoration on top
-st.components.v1.html(
-    """
-    <script>
-    // Modify the decoration on top to reuse as a banner
-
-    // Locate elements
-    var decoration = window.parent.document.querySelectorAll('[data-testid="stDecoration"]')[0];
-    var sidebar = window.parent.document.querySelectorAll('[data-testid="stSidebar"]')[0];
-
-    // Observe sidebar size
-    function outputsize() {
-        decoration.style.left = `${sidebar.offsetWidth}px`;
-    }
-
-    new ResizeObserver(outputsize).observe(sidebar);
-
-    // Adjust sizes
-    outputsize();
-    decoration.style.height = "3.0rem";
-    decoration.style.right = "45px";
-
-    // Adjust image decorations
-    decoration.style.backgroundImage = "url(https://img.pokemondb.net/sprites/emerald/normal/pikachu.png)";
-    decoration.style.backgroundSize = "contain";
-    </script>        
-    """, width=0, height=0)
 
 # Data inladen
 bestanden = ['2021_Q2_Central.csv', '2021_Q3_Central.csv', '2021_Q4_Central.csv']
